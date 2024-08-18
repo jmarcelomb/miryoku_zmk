857a2c7ee855b105b3459eb37b25d6e03ac53c69
Marcelo Borges <jmmb1098@gmail.com>
2024-08-18 16:35:30 +0100

    Removes undef

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,6 @@
 // https://github.com/manna-harbour/miryoku
 
 // MIRYOKU_ALTERNATIVES_FUN_FLIP
-#undef MIRYOKU_LAYER_FUN
 #define MIRYOKU_LAYER_FUN \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PSCRN,         &kp F7,            &kp F8,            &kp F9,            &kp F12,           \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp SLCK,          &kp F4,            &kp F5,            &kp F6,            &kp F11,           \
@@ -10,7 +9,6 @@ U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp SPACE,         &kp K_APP,         U_NP,              U_NP
 
 // MIRYOKU_ALTERNATIVES_MOUSE_FLIP
-#undef MIRYOKU_LAYER_MOUSE
 #define MIRYOKU_LAYER_MOUSE \
 U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
