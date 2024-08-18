4e9303526d40db11885be1bf87c72f67170469a5
Marcelo Borges <jmmb1098@gmail.com>
2024-08-18 18:16:55 +0100

    Updates layers in MIRYOKU_BASE

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,6 +12,13 @@ K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
 K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
                K33  K34       K35  K36
 
+// MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH
+#define MIRYOKU_BASE \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_SYM, SPACE),U_LT(U_NUM, TAB),U_LT(U_NAV, RET),  U_LT(U_FUN, BSPC), U_LT(U_MOUSE, DEL),  U_NP,              U_NP
+
 
 // MIRYOKU_ALTERNATIVES_NUM_FLIP
 #define MIRYOKU_LAYER_NUM \
