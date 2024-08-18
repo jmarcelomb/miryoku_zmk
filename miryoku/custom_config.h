9e1db67d4bdd270f70eb535a570b3a298865ab34
Marcelo Borges <jmmb1098@gmail.com>
2024-08-18 17:15:56 +0100

    Inverts num and nav, adds combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,40 +1,84 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// MIRYOKU_ALTERNATIVES_FUN_FLIP
-#define MIRYOKU_LAYER_FUN \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PSCRN,         &kp F7,            &kp F8,            &kp F9,            &kp F12,           \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp SLCK,          &kp F4,            &kp F5,            &kp F6,            &kp F11,           \
-U_NA,              &kp RALT,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA,              &kp PAUSE_BREAK,   &kp F1,            &kp F2,            &kp F3,            &kp F10,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp SPACE,         &kp K_APP,         U_NP,              U_NP
+#define MIRYOKU_LAYOUTMAPPING_SWEEP( \
+K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+K00  K01  K02  K03  K04       K05  K06  K07  K08  K09 \
+K10  K11  K12  K13  K14       K15  K16  K17  K18  K19 \
+K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
+               K33  K34       K35  K36
+
+
+// MIRYOKU_ALTERNATIVES_NUM_FLIP
+#define MIRYOKU_LAYER_NUM \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp EQUAL,         &kp N4,            &kp N5,            &kp N6,            &kp SEMI,          \
+U_NA,              &kp RALT,          &u_to_U_NAV,       &u_to_U_NUM,       U_NA,              &kp BSLH,          &kp N1,            &kp N2,            &kp N3,            &kp GRAVE,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp MINUS,         &kp N0,            &kp DOT,           U_NP,              U_NP
+
+// MIRYOKU_ALTERNATIVES_SYM_FLIP
+#define MIRYOKU_LAYER_SYM \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp PLUS,          &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp COLON,         \
+U_NA,              &kp RALT,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp PIPE,          &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp UNDER,         &kp LPAR,          &kp RPAR,          U_NP,              U_NP
 
 // MIRYOKU_ALTERNATIVES_MOUSE_FLIP
-#define MIRYOKU_LAYER_MOUSE \
+#define MIRYOKU_LAYER_MOUSE MIRYOKU_ALTERNATIVES_MOUSE_FLIP
 U_WH_L,            U_WH_D,            U_WH_U,            U_WH_R,            U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_NA,              &u_to_U_MOUSE,     &u_to_U_SYM,       &kp RALT,          U_NA,              \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
-
 
 #define MIRYOKU_LAYER_LIST \
 MIRYOKU_X(BASE,   "Base") \
 MIRYOKU_X(EXTRA,  "Extra") \
 MIRYOKU_X(TAP,    "Tap") \
 MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(FUN,    "Fun") \
-MIRYOKU_X(MEDIA,  "Media") \
 MIRYOKU_X(NUM,    "Num") \
 MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(FUN,    "Fun") \
 MIRYOKU_X(MOUSE,  "Mouse")
 
 #define U_BASE   0
 #define U_EXTRA  1
 #define U_TAP    2
 #define U_BUTTON 3
-#define U_NAV    4
-#define U_FUN    5
+#define U_NUM    4
+#define U_SYM    5
 #define U_MEDIA  6 
-#define U_NUM    7
-#define U_SYM    8
+#define U_NAV    7
+#define U_FUN    8
 #define U_MOUSE  9
+
+/ {
+    combos {
+        compatible = "zmk,combos";
+        combo_esc {
+            timeout-ms = <50>;
+            key-positions = <1 2>;
+            bindings = <&kp ESC>;
+         };
+        combo_bspc {
+            timeout-ms = <50>;
+            key-positions = <7 8>;
+            bindings = <&kp BSPC>;
+        };
+        combo_dot_comma {
+            timeout-ms = <50>;
+            key-positions = <27 28>;
+            bindings = <&kp SEMICOLON>;
+        };
+        combo_del {
+            timeout-ms = <50>;
+            key-positions = <8 9>;
+            bindings = <&kp DEL>;
+        };
+    };
+};
