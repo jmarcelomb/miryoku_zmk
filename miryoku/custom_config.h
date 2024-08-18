b9f918c6bad4cc48542c834f2eaf40690eb74f6f
Marcelo Borges <jmmb1098@gmail.com>
2024-08-18 18:28:29 +0100

    Fix base layer name typo

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -13,7 +13,7 @@ K20  K21  K22  K23  K24       K25  K26  K27  K28  K29 \
                K33  K34       K35  K36
 
 // MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH
-#define MIRYOKU_BASE \
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
